95550060d7b081813e89669dfbfa82d5aeccc285
anatoly yakovenko <anatoly@solana.com>
2026-06-23 09:04:50 +0000

    Guard InitPortfolio final shape

--- src/v16_program.rs
+++ src/v16_program.rs
@@ -5586,6 +5586,7 @@ pub mod processor {
             group
                 .register_empty_materialized_portfolio_not_atomic(&portfolio.as_view())
                 .map_err(map_v16_error)?;
+            group.validate_shape().map_err(map_v16_error)?;
         }
         let _ = (cfg, source_domain_count);
         Ok(())
